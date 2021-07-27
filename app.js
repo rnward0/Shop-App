a892d0eb99ef46b77b31f07f3139443680fa4df1
rnward0 <49202674+rnward0@users.noreply.github.com>
2021-07-27 12:49:41 -0600

    Fixes

--- app.js
+++ app.js
@@ -7,18 +7,17 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const pageNotFoundController = require('./controllers/page-not-found');
-const db = require('./util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views'); //Don't need to add this if the views are in a folder called views, otherwise must register the actual folder name
 
-app.use('/admin', adminRoutes); //adminRoutes using Router acts as valid middleware, so we can simply call it like this
-app.use(shopRoutes); //Same with shopRoutes
-
 app.use(bodyParser.urlencoded({ extended: false })); //Parses body of requests, then calls next() method to continue to other middleware
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes); //adminRoutes using Router acts as valid middleware, so we can simply call it like this
+app.use(shopRoutes); //Same with shopRoutes
 
 app.use(pageNotFoundController.getPageNotFound);
 
